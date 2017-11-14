790c8e1e56c3e3fd5b939822e70fada408a29d0f
Администратор <Starternh@gmail.com>
2017-11-14 21:47:03 +0300

    First

--- index.php
+++ index.php
@@ -5,31 +5,5 @@ error_reporting(E_ALL);
 
 define('ROOT', dirname(__FILE__));
 
-class ixtFiles
-{
-    private function filterByExtension($var)
-    {
-        return preg_match("/^[a-zа-яё\d]{1}[a-zа-яё\d\s]*\.{1}\ixt$/i", $var);
-    }
-    private function isDir($var)
-    {
-        return !is_dir(ROOT."/ixtfolder/$var");
-    }
 
-    public function findFiles()
-    {
-        $scanned_directory = array_diff(scandir(ROOT . '/ixtfolder'), array('..', '.'));
-        $result = array_filter($scanned_directory, "self::filterByExtension");
-        $result = array_filter($result, "self::isDir");
-        sort($result);
-
-        foreach ($result as $key => $value) {
-            echo $value;
-            echo "<br>";
-        }
-    }
-}
-
-$a = new ixtFiles();
-$a->findFiles();
 ?>
